a334962438876cd29a3174c0d592957e521a3dc3
Jeremy Zongker <jeremy@zongker.net>
2025-08-22 02:33:44 -0500

    Fixed esmodule incompatibility issues

--- index.js
+++ index.js
@@ -13,7 +13,13 @@
   limitations under the License.
 */
 
-const path = require("path")
+import path from "path";
+import { fileURLToPath } from "url";
+import { createRequire } from "module";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const require = createRequire(import.meta.url);
 
 const addon = require('bindings')({
   bindings: "grandiose",
